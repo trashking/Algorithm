76508a9a48e0d53694b3e5e588cc1f699297c9d5
ADI\1607002 <receipt.defy@gmail.com>
2017-11-22 17:39:40 +0900

    BOJ_2441 1h 12m 5a1238c543aa5e822b8452aa

---
+++ src/BOJ_2441/Main.java
@@ -0,0 +1,43 @@
+/*
+* 문제
+* 첫째 줄에는 별 N개, 둘째 줄에는 별 N-1개, ..., N번째 줄에는 별 1개를 찍는 문제
+*
+* 하지만, 오른쪽을 기준으로 정렬한 별 (예제 참고)을 출력하시오.
+*
+* 입력
+* 첫째 줄에 N (1<=N<=100)이 주어진다.
+*
+* 출력
+* 첫째 줄부터 N번째 줄 까지 차례대로 별을 출력한다.
+*
+* 예제 입력
+* 5
+*
+* 예제 출력
+* *****
+*  ****
+*   ***
+*    **
+*     *
+
+ */
+
+package BOJ_2441;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+
+		for (int N = sc.nextInt(), i = N; i > 0; i--) {
+			for (int j = 0; j <= N - (i + 1); j++) {
+				System.out.print(" ");
+			}
+			for (int k = 0; k < i; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
