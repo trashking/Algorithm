ae211743faa82c2429f837cbef11a009111b10e9
ADI\1607002 <receipt.defy@gmail.com>
2017-11-23 15:13:01 +0900

    BOJ_2442 1h 0m 5a1238c749e485f38740d673

--- src/BOJ_2442/Main.java
+++ src/BOJ_2442/Main.java
@@ -23,5 +23,21 @@
 
 package BOJ_2442;
 
+import java.util.Scanner;
+
 public class Main {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+
+		for (int N = sc.nextInt(), i = 1; i <= N; i++) {
+			for (int j = 1; j <= N - i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= 2 * i - 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
 }
