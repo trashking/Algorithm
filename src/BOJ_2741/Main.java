56e5776a008a794b5f9a50b21ab712feaf1d2b1b
ADI\1607002 <receipt.defy@gmail.com>
2017-12-15 16:51:24 +0900

    입/출력 N찍기 다시 해봄

--- src/BOJ_2741/Main.java
+++ src/BOJ_2741/Main.java
@@ -6,10 +6,18 @@ public class Main {
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 
-		int a = sc.nextInt();
+		int n = sc.nextInt();
 
-		for (int i = 1; i <= a; i++) {
-			System.out.println(i);
+		//for (int i = 1; i <= n; i++) {
+		//	System.out.println(i);
+		//}
+		// 알고리즘 입/출력에서 나온 stringbuilder 로 변경해봄
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 1; i <= n; i++) {
+			sb.append(i + "\n");
 		}
+
+		System.out.print(sb);
 	}
 }
